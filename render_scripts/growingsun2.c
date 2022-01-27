56877d280e62c38793456ca5999e268d8d2898b3
rowedaniel <rowedanielp@gmail.com>
2022-01-27 09:48:53 -0800

    reorganization and matrix tools

---
+++ render_scripts/growingsun2.c
@@ -0,0 +1,62 @@
+
+
+
+# include "FPToolkit.c"
+
+
+int main(int argc, char *argv[])
+{
+	
+	// arguments:
+	int screen_width, screen_height;
+	int framecount;
+	double growth;
+	
+	// other vars:
+	double centerx, centery;
+	double rad;
+	char filename[100];
+	double color[3];
+	int i;
+
+	if(argc != 5) {
+		printf("Usage: pgm_name screen_width screen_height num_frames delta_radius\n");
+		return 0;
+	}
+
+
+	screen_width = atoi(argv[1]); screen_height = atoi(argv[2]);
+	framecount = atoi(argv[3]);
+	growth = atof(argv[4]);
+
+	centerx = screen_width / 2.0;
+	centery = screen_height / 2.0;
+	rad = 10;
+	color[0] = 1;
+	color[1] = 0;
+	color[2] = 0;
+
+	G_init_graphics(screen_width, screen_height);
+
+
+	for(i = 0; i < framecount; ++i) {
+		G_rgb(0, 0, 0);
+		G_clear();
+
+		color[1] = i * 1.0/framecount;
+		rad = 10 + i * growth;
+		for(int j = i; j > 0; --j) {
+			G_rgb(color[0], color[1], color[2]);
+			G_fill_circle(centerx, centery, rad);
+			rad -= growth;
+			color[1] -= 1.0 / framecount;
+		}
+
+		G_display_image();
+		sprintf(filename, "growingsun%04d.xwd", i);
+		G_save_image_to_file(filename);
+
+	}
+
+
+}
---
+++ stickfigureS.c
@@ -0,0 +1,110 @@
+
+#include "FPToolkit.c"
+#include "M3d_matrix_tools.c"
+#include "time.h"
+
+// stickfigure initially designed as centered for a 400x400 window :
+double x[13] = {175,225,225,300,225,225,250,200,150,175,175,100,175} ;
+double y[13] = {300,300,250,225,225,200,100,175,100,200,225,225,250} ;
+double z[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0} ;
+       // z[] values unimportant but should NOT be left uninitialized
+       // as nan values WILL propagate through
+int n = 13 ;
+
+
+
+int main(int argc, char **argv) 
+{
+
+ if (argc != 3) {
+    printf("usage : pgm_name   window_size  microseconds(30000)\n") ;
+    exit(0) ;
+ }
+  
+ double winsize = atof(argv[1]) ;
+ double u = atof(argv[2]) ; 
+
+ G_init_graphics(winsize,winsize) ;
+
+
+ // the original design was for a 400x400
+ // window and the object is centered on 200,200
+ // so we recenter it and make it larger
+ // (you get to do this ... use the
+ // M3d_make_movement_sequence_matrix  function :)
+
+ // .....
+
+ double v[4][4], vi[4][4];
+ int i ;
+ int mtype[100] ;
+ double mparam[100] ;
+
+ i = 0 ;
+ mtype[i] = TX ;  mparam[i] =  -200      ; i++ ;
+ mtype[i] = TY ;  mparam[i] =  -200      ; i++ ;
+ mtype[i] = SX ;  mparam[i] =  winsize / 400.0 ; i++ ;
+ mtype[i] = SY ;  mparam[i] =  winsize / 400.0 ; i++ ;
+ mtype[i] = TX ;  mparam[i] =  winsize / 2.0   ; i++ ;
+ mtype[i] = TY ;  mparam[i] =  winsize / 2.0   ; i++ ;
+
+ M3d_make_movement_sequence_matrix(v,vi,  i,mtype,mparam) ;
+
+ M3d_mat_mult_points(x, y, z, v, x, y, z, n);
+
+ printf("1st mat:\n");
+ M3d_print_mat(v);
+ 
+ // now make the movie the rotates and shrinks about the center :
+
+ i = 0 ;
+ mtype[i] = TX ;  mparam[i] =  -winsize/2.0 ; i++ ;
+ mtype[i] = TY ;  mparam[i] =  -winsize/2.0 ; i++ ;
+ mtype[i] = RZ ;  mparam[i] =        1.0    ; i++ ;
+ mtype[i] = SX ;  mparam[i] =        0.98   ; i++ ;
+ mtype[i] = SY ;  mparam[i] =        0.98   ; i++ ;
+ mtype[i] = TX ;  mparam[i] =  winsize/2.0  ; i++ ;
+ mtype[i] = TY ;  mparam[i] =  winsize/2.0  ; i++ ;
+
+ M3d_make_movement_sequence_matrix(v,vi,  i,mtype,mparam) ;
+
+ printf("2nd mat:\n");
+ M3d_print_mat(v);
+
+ // .....
+ 
+ G_rgb(0,0,0);
+ G_clear();
+ G_rgb(1,1,1);
+ G_fill_polygon(x,y,n);
+ G_wait_key();
+ 
+
+ printf("frametime: %lf\n", u);
+ for(i=0; i<360*3; ++i) { 
+
+   G_rgb(0,0,0);
+   G_clear();
+
+   G_rgb(0.5, 0.4, 0.2);
+   G_fill_polygon(x, y, n);
+   M3d_mat_mult_points(x, y, z, v, x, y, z, n);
+
+   G_display_image();
+
+   /*
+   if(G_no_wait_key() == 'q') {
+     break;
+   }
+   */
+
+   usleep(u);
+
+ }
+
+ G_rgb(1, 0, 0);
+ G_fill_circle(winsize/2.0, winsize/2.0, winsize/4.0);
+ G_wait_key();
+
+}
+
