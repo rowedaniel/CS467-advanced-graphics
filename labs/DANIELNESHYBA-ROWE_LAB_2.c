a84506841a1ae214f63ef9809882b5b8f5fcae33
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2022-03-01 14:38:12 -0800

    added lab 2 to folder

---
+++ labs/DANIELNESHYBA-ROWE_LAB_2.c
@@ -0,0 +1,542 @@
+#include "graph3D_no_window.c"
+
+
+
+// declare global vars
+
+#define M 100
+double numobjects = 0;
+int color_type[M];
+int image_IDs[M];
+int image_size[M][2];
+double inherent_rgb[M][3];
+
+int id; // id for xwd tools
+
+
+
+
+
+
+void draw_all_objects(double V[4][4], double T[M][4][4],
+		int (*f[M]) (double u, double v, double P[3]),
+		double uStart[M], double uEnd[M], double uStep[M],
+		double vStart[M], double vEnd[M], double vStep[M]
+		)
+{
+  double t[4][4];
+
+  for(int onum = 0; onum < numobjects; ++onum) {
+
+    M3d_mat_mult(t, V, T[onum]);
+    if(color_type[onum]) {
+    	make_graph_inherentrgb_1func(
+			uStart[onum], uEnd[onum], uStep[onum],
+	       		vStart[onum], vEnd[onum], vStep[onum],
+    	       		f[onum], 
+               		t,
+	       		inherent_rgb[onum],
+			id
+	       		);
+    } else {
+    	make_graph_image_1func(
+			uStart[onum], uEnd[onum], uStep[onum],
+	       		vStart[onum], vEnd[onum], vStep[onum],
+    	       		f[onum], 
+               		t,
+	       		image_IDs[onum], image_size[onum][0], image_size[onum][1],
+			id
+	       		);
+    }
+
+  }
+}
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+int main ()
+{
+  int onum;
+
+  // stuff for moving each object to the right location
+  double V[M][4][4], Vi[M][4][4];
+  int nl ;
+  int tlist[100] ;
+  double plist[100] ;
+  double final_trans[4][4], final_transi[4][4];
+  double movement_trans[4][4], movement_transi[4][4];
+  // array of generation functions for each shape
+  int (*f[M]) (double u, double v, double P[3]);
+
+  // array of start/stop/step values for each parameterization
+  double uStart[M], uEnd[M], uStep[M];
+  double vStart[M], vEnd[M], vStep[M];
+
+  // stuff for loading image files
+  int error;
+
+
+
+  // initialize pointCloud stuff
+  make_win_transform(600, 600);
+  // init graphics
+  id = create_new_xwd_map(win_width, win_height);
+  if(id == -1) { printf("failure!\n"); exit(0); }
+
+
+
+
+  //---------------------------------------------------------------------
+  // Design the model by moving objects from their own OBJECT SPACE 
+  // to a common WORLD SPACE :
+  //---------------------------------------------------------------------
+
+  onum = 0 ;  // current object number
+
+  // The sphere has a radius of 1.0, centered at the origin.
+  int sphere(double u, double v, double P[3]) {
+	  const double a = 1-v*v;
+	  if(a<0) { return 0; }
+	  const double b = sqrt(a);
+  	  P[0] = b*cos(u);
+      	  P[1] =  v;
+      	  P[2] = b*sin(u);
+    	  return 1;
+  }
+
+  // "Torus" has center radius 1, outer radius 0.1
+  int torus(double u, double v, double P[3]) {
+	  const double torus_centerR = 1;
+	  const double torus_outerR = 0.1 / 2.0;
+	  const double torus_squareness = 1.0 / 3.0;
+
+	  if(v >= 8) { return 0; }
+
+
+	  double r, z;
+	  {
+	  const double c = cos(M_PI/2.0 * v);
+	  const double s = sin(M_PI/2.0 * v);
+	  if(0 <= v && v < 1) {
+		  r = 2;
+		  z = 2*v-1;
+	  } else if (1 <= v && v < 2) {
+		  r = 1+s;
+		  z = 1-c;
+	  } else if(2 <= v && v < 3) {
+		  r = 5-2*v;
+		  z = 2;
+	  } else if(3 <= v && v < 4) {
+		  r = -1-c;
+		  z = 1-s;
+	  } else if(4 <= v && v < 5) {
+		  r = -2;
+		  z = 9-2*v;
+	  } else if(5 <= v && v < 6) {
+		  r = -1-s;
+		  z = -1+c;
+	  } else if(6 <= v && v < 7) {
+		  r = -13+2*v;
+		  z = -2;
+	  } else if(7 <= v && v <= 8) {
+		  r = 1+c;
+		  z = -1+s;
+	  }
+	  }
+
+
+	  P[0] = (torus_centerR + torus_outerR*r) * cos(u);
+	  P[1] = (torus_centerR + torus_outerR*r) * sin(u);
+	  P[2] = torus_outerR*z;
+	  return 1;
+  }
+
+
+  // scaffolding is same as torus, but different parameterization (makes it look cooler)
+  int scaffolding(double u, double v, double P[3]) {
+	  const double torus_centerR = 1;
+	  const double torus_outerR = 0.08;
+	  const double torus_squareness = 1.0 / 3.0;
+	  double f(double x) {
+	  	double a = cos(x);
+	  	return ((a>0)-(a<0)) * pow(fabs(a), torus_squareness);
+	  }
+	  P[0] = (torus_centerR + torus_outerR*f(v)) * cos(u);
+	  P[1] = (torus_centerR + torus_outerR*f(v)) * sin(u);
+	  P[2] = torus_outerR*sin(v);
+	  return 1;
+  }
+  
+  // The cylinder has radius of 0.1, centered at origin, 
+  // lying on the x-axis from x = -1 to x = 1.
+  int cylinder(double u, double v, double P[3]) {
+	  double rad = 0.1;
+  	  P[0] = -u;
+  	  P[1] =  rad*cos(v);
+  	  P[2] =  rad*sin(v);
+	  return 1;
+  }
+
+  // The cone has radius of 0.1, centered at origin, 
+  // lying on the x-axis from x = 0 to x = 1.
+  int cone(double u, double v, double P[3]) {
+	  double rad = 0.1;
+  	  P[0] = -u;
+  	  P[1] =  rad*u*cos(v);
+  	  P[2] =  rad*u*sin(v);
+	  return 1;
+  }
+
+  // door bays are circles (filled in), but with a rectangular slot cut out
+  int doorbay(double u, double v, double P[3]) {
+	  const double doorbay_angle = M_PI/9;
+	  const double doorbay_inset = 0.8;
+	  const double doorbay_x = doorbay_inset * cos(doorbay_angle);
+	  const double doorbay_y = doorbay_inset * sin(doorbay_angle);
+
+	  const double x=v*cos(u);
+	  const double y=v*sin(u);
+
+	  if(-doorbay_x < x && x < doorbay_x && -doorbay_y < y && y < doorbay_y) {
+	    return 0;
+	  }
+
+	  P[0] =  x;
+	  P[1] =  y;
+	  P[2] =  0;
+	  return 1;
+  }
+
+
+  double station_color[3] = {0.6, 0.6, 0.9};
+  double station_highlight[3] = {0.7, 0.3, 0.4};
+
+  double planetres = 0.01; //0.001;
+  double highres = 0.01; //0.005;
+  double lowres = 0.2;
+
+
+
+  // build planet
+  image_IDs[onum] = init_xwd_map_from_file("earth_jeff.xwd");
+  if(image_IDs[onum] == -1) { printf("File load failure!\n"); exit(0); }
+  error = get_xwd_map_dimensions(image_IDs[onum],image_size[onum]); 
+  if(error == -1) { printf("File load failure!\n"); exit(0); }
+
+  nl = 0 ;
+  tlist[nl] = RX ; plist[nl] = -45.00   ; nl++ ;
+  tlist[nl] = RY ; plist[nl] =  10.00   ; nl++ ;
+  tlist[nl] = TX ; plist[nl] =  0.90    ; nl++ ;
+  tlist[nl] = TY ; plist[nl] = -0.50    ; nl++ ;
+  tlist[nl] = TZ ; plist[nl] = -1.10    ; nl++ ;
+  tlist[nl] = SX ; plist[nl] = -140.00  ; nl++ ;
+  tlist[nl] = SY ; plist[nl] = -140.00  ; nl++ ;
+  tlist[nl] = SZ ; plist[nl] = -140.00  ; nl++ ;
+  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+  
+  f[onum] = sphere; 
+
+  uStart[onum] = 0;		uEnd[onum] = 2*M_PI, 	 uStep[onum] = planetres;
+  vStart[onum] = -M_PI/2;	vEnd[onum] = M_PI/2,	 vStep[onum] = planetres;
+
+  onum++ ;
+
+
+  
+  // Build central axis cylinder.
+  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  color_type[onum] = 1;
+
+  nl = 0 ;
+  tlist[nl] = SX ; plist[nl] = 2.00 ; nl++ ;
+  tlist[nl] = SY ; plist[nl] = 6.00 ; nl++ ;
+  tlist[nl] = SZ ; plist[nl] = 6.00 ; nl++ ;
+  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+  
+  f[onum] = cylinder; 
+
+  uStart[onum] = -1;		uEnd[onum] = 1, 	 uStep[onum] = highres;
+  vStart[onum] = 0;		vEnd[onum] = 2*M_PI,	 vStep[onum] = highres;
+
+  onum++ ;
+
+
+  // Build radial torus
+  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  color_type[onum] = 1;
+
+  nl = 0 ;
+  tlist[nl] = SX ; plist[nl] = 4.00 ; nl++ ;
+  tlist[nl] = SY ; plist[nl] = 4.00 ; nl++ ;
+  tlist[nl] = SZ ; plist[nl] = 4.00 ; nl++ ;
+  tlist[nl] = RY ; plist[nl] = 90.0 ; nl++ ;
+  tlist[nl] = TX ; plist[nl] = 2.00 ; nl++ ;
+  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+  
+  f[onum] = torus; 
+
+  uStart[onum] = 0;		uEnd[onum] = 2*M_PI;	uStep[onum] = highres;
+  vStart[onum] = 0;		vEnd[onum] = 8;		vStep[onum] = highres;
+  onum++ ;
+
+
+  // build scaffolding
+
+  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_highlight[c];}
+  color_type[onum] = 1;
+
+  nl = 0 ;
+  tlist[nl] = SX ; plist[nl] = 4.00 ; nl++ ;
+  tlist[nl] = SY ; plist[nl] = 4.00 ; nl++ ;
+  tlist[nl] = SZ ; plist[nl] = 4.00 ; nl++ ;
+  tlist[nl] = RY ; plist[nl] = 90.0 ; nl++ ;
+  tlist[nl] = TX ; plist[nl] = -2.00 ; nl++ ;
+  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+  
+  f[onum] = scaffolding; 
+
+  uStart[onum] = 0;		uEnd[onum] = 2*M_PI;	uStep[onum] = lowres;
+  vStart[onum] = 0;		vEnd[onum] = 8;		vStep[onum] = lowres;
+
+  onum++ ;
+
+  for(int i=0; i<4; ++i) {
+	  // built sections of scaffold
+	  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  	  color_type[onum] = 1;
+
+	  nl = 0 ;
+	  tlist[nl] = SX ; plist[nl] = 4.00 ; nl++ ;
+	  tlist[nl] = SY ; plist[nl] = 4.00 ; nl++ ;
+	  tlist[nl] = SZ ; plist[nl] = 4.00 ; nl++ ;
+	  tlist[nl] = RY ; plist[nl] = 90.0 ; nl++ ;
+	  tlist[nl] = RX ; plist[nl] = i*90.0 ; nl++ ;
+	  tlist[nl] = TX ; plist[nl] = -2.00 ; nl++ ;
+	  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+	  
+	  f[onum] = torus; 
+
+	  uStart[onum] = -M_PI/10.0;	uEnd[onum] = M_PI/10.0;	uStep[onum] = highres;
+	  vStart[onum] = 0;		vEnd[onum] = 8;		vStep[onum] = highres;
+
+	  onum++ ;
+  }
+
+
+
+  for(int i=0; i<2; ++i) {
+
+
+	  // Build larger cylinder ends
+	  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  	  color_type[onum] = 1;
+	
+	  nl = 0 ;
+	  tlist[nl] = SY ; plist[nl] = 10.00 ; nl++ ;
+	  tlist[nl] = SZ ; plist[nl] = 10.00 ; nl++ ;
+	  tlist[nl] = SX ; plist[nl] = 0.50 ; nl++ ;
+	  tlist[nl] = TX ; plist[nl] = 2.00*(i*2-1) ; nl++ ;
+	  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+	  
+	  f[onum] = cylinder;
+	
+	  uStart[onum] = -1;		uEnd[onum] = 1,		 uStep[onum] = highres;
+	  vStart[onum] = 0;		vEnd[onum] = 2*M_PI,	 vStep[onum] = highres;
+	
+	  onum++ ;
+
+	  // build cone connecters for larger ends
+	  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  	  color_type[onum] = 1;
+	
+	  nl = 0 ;
+	  tlist[nl] = SY ; plist[nl] = 10.00 ; nl++ ;
+	  tlist[nl] = SZ ; plist[nl] = 10.00 ; nl++ ;
+	  tlist[nl] = SX ; plist[nl] = 1.00  ; nl++ ;
+	  tlist[nl] = TX ; plist[nl] = 0.50  ; nl++ ;
+	  tlist[nl] = RY ; plist[nl] = 180.0*i ; nl++ ;
+	  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+	  
+	  f[onum] = cone;
+	
+	  uStart[onum] = -1;		uEnd[onum] = 0,		 uStep[onum] = highres;
+	  vStart[onum] = 0;		vEnd[onum] = 2*M_PI,	 vStep[onum] = highres;
+	
+	  onum++ ;
+
+
+
+	  // Build doorbays
+	  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  	  color_type[onum] = 1;
+	
+	  nl = 0 ;
+	  tlist[nl] = SY ; plist[nl] = 1.00 ; nl++ ;
+	  tlist[nl] = SX ; plist[nl] = 1.00 ; nl++ ;
+	  tlist[nl] = RY ; plist[nl] = -90.0; nl++ ;
+	  tlist[nl] = TX ; plist[nl] = 2.50 ; nl++ ;
+	  tlist[nl] = RY ; plist[nl] = 180.0*i; nl++ ;
+	  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+
+	  
+       	  f[onum] = doorbay;
+	  
+	  uStart[onum] = 0;		uEnd[onum] = 2*M_PI,	 uStep[onum] = highres/10;
+	  vStart[onum] = 0;		vEnd[onum] = 1,		 vStep[onum] = highres/10;
+	
+	  onum++ ;
+
+
+
+	  // Build 'spokes'
+	  for(int j=0; j<4; ++j) {
+		  for(int c=0;c<3;++c) {inherent_rgb[onum][c]=station_color[c];}
+  		  color_type[onum] = 1;
+		
+		  nl = 0 ;
+		  //tlist[nl] = TZ ; plist[nl] = 1.00 ; nl++ ;
+		  tlist[nl] = SX ; plist[nl] = 1.50 ; nl++ ;
+		  tlist[nl] = SY ; plist[nl] = 4.00 ; nl++ ;
+		  tlist[nl] = SZ ; plist[nl] = 4.00 ; nl++ ;
+		  tlist[nl] = RY ; plist[nl] = 90.0 ; nl++ ;
+		  tlist[nl] = TZ ; plist[nl] = 2.50 ; nl++ ;
+		  tlist[nl] = RX ; plist[nl] = 90.00*j ; nl++ ;
+		  tlist[nl] = TX ; plist[nl] = 2.00*(i*2-1) ; nl++ ;
+		  M3d_make_movement_sequence_matrix (V[onum],Vi[onum],  nl,tlist,plist) ;
+		  
+		  f[onum] = cylinder; 
+		
+		  uStart[onum] = -1;		uEnd[onum] = 1,		 uStep[onum] = highres;
+		  vStart[onum] = 0;		vEnd[onum] = 2*M_PI,	 vStep[onum] = highres;
+		
+		  onum++ ;
+
+	  }
+  }
+
+  // transform all the space-station objects to be in a reasonable position in worldspace
+  nl = 0 ;
+  tlist[nl] = RX ; plist[nl] = -55.0 ; nl++ ;
+  tlist[nl] = RY ; plist[nl] = -50.0 ; nl++ ;
+  tlist[nl] = RZ ; plist[nl] = 10.0 ; nl++ ;
+  M3d_make_movement_sequence_matrix (final_trans,final_transi,  nl,tlist,plist) ;
+
+
+
+
+
+
+
+
+
+
+
+
+  //---------------------------------------------------------------------
+  numobjects = onum ;
+  //---------------------------------------------------------------------
+
+
+  // object movement stuff
+  double final_V[M][4][4];
+
+  // eye stuff
+  double eye[3], coi[3], up[3] ;
+  double v[4][4], vi[4][4];
+  int i;
+
+  int fnum ;
+  int max_fnum;
+  double t ;
+
+  fnum = 0 ;
+  max_fnum = 360;
+
+
+  // light model setup
+  double light_in_world_space[3] = {5,  1, 0};
+  
+  AMBIENT = 0.2 ;
+  MAX_DIFFUSE = 0.5 ;
+  SPECPOW = 30 ;
+
+  // file saving setup
+  char filename[100];
+
+
+  while (1) {
+
+    t = 0.05*fnum; //0.003333*fnum ;
+
+    eye[0] = 15-t*10; //15(2*M_PI*t) ; 
+    eye[1] =  0; //6*t ; 
+    eye[2] =  5; //7*sin(2*M_PI*t) ; 
+
+    coi[0] =  cos(2*M_PI*t) ;
+    coi[1] =  2.0 ; 
+    coi[2] =  sin(2*M_PI*t);
+
+    up[0]  = eye[0] ; 
+    up[1]  = eye[1] + 1 ;
+    up[2]  = eye[2] ; 
+
+    //------------------------------- put your code here!!!!!!!!!!!!
+
+    // clear the screen
+    for(int x=0; x<win_width; ++x) {
+    	for(int y=0; y<win_width; ++y) {
+	    set_xwd_map_color(id, x,y, 0.0, 0.0, 0.0);
+	}
+    }
+    printf("rendering frame %d\n", fnum);
+
+
+    // render
+    M3d_view(v,vi, eye, coi, up);
+
+    M3d_mat_mult_pt(light_in_eye_space, v, light_in_world_space);
+    
+    
+    // update space station rotation
+    nl = 0 ;
+    tlist[nl] = RX ; plist[nl] = t*100 ; nl++ ;
+    M3d_make_movement_sequence_matrix (movement_trans,movement_transi,  nl,tlist,plist) ;
+
+    // copy transformation over for planet (it doesn't move)
+    M3d_copy_mat(final_V[0], V[0]);
+    // update transformation for moving objects (not obj 0--the planet)
+    for(int i=1; i<onum; ++i) {
+	    M3d_mat_mult(final_V[i], final_trans, movement_trans);
+	    M3d_mat_mult(final_V[i], final_V[i], V[i]);
+    }
+
+    make_z_buff();
+    draw_all_objects(v, final_V,
+		     f,
+		     uStart, uEnd, uStep,
+		     vStart, vEnd, vStep
+		     );
+
+
+
+
+
+
+    sprintf(filename, "pointcloudimg2%04d.xwd", fnum);
+    xwd_map_to_named_xwd_file(id, filename);
+
+
+
+    fnum++ ;
+    if(fnum > max_fnum) {
+	    break;
+    }
+  } // end while (1)
+
+
+
+}
+
---
+++ labs/graph3D_no_window.c
@@ -0,0 +1,475 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "M3d_matrix_tools.c"
+#include "xwd_tools_03.c"
+
+
+#define MAX_WIN_WIDTH 2000
+#define MAX_WIN_HEIGHT 2000
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// To support the light model :
+double light_in_eye_space[3] ;
+double AMBIENT      = 0.2 ;
+double MAX_DIFFUSE  = 0.5 ;
+double SPECPOW      = 50 ;
+
+
+
+int Light_Model (double irgb[3],
+                 double s[3],
+                 double p[3],
+                 double n[3],
+                 double argb[3])
+// s,p,n in eyespace
+
+// irgb == inherent color of object (input to this function)
+// s = location of start of ray (probably the eye)
+// p = point on object (input to this function)
+// n = normal to the object at p (input to this function)
+// argb == actual color of object (output of this function)
+// globals : AMBIENT, MAX_DIFFUSE, SPECPOW, light_in_eye_space[3]
+
+// return 1 if successful, 0 if error
+{
+
+  double len ;
+  double N[3] ; 
+  len = sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]) ;
+  if (len == 0) return 0 ;
+  N[0] = n[0]/len ;  N[1] = n[1]/len ;  N[2] = n[2]/len ;
+
+  double E[3] ;
+  E[0] = s[0] - p[0] ; 
+  E[1] = s[1] - p[1] ; 
+  E[2] = s[2] - p[2] ; 
+  len = sqrt(E[0]*E[0] + E[1]*E[1] + E[2]*E[2]) ;
+  if (len == 0) return 0 ;
+  E[0] /= len ;  E[1] /= len ;  E[2] /= len ;
+  double NdotE = N[0]*E[0] + N[1]*E[1] + N[2]*E[2] ;
+
+  double L[3] ;
+  L[0] = light_in_eye_space[0] - p[0] ; 
+  L[1] = light_in_eye_space[1] - p[1] ; 
+  L[2] = light_in_eye_space[2] - p[2] ; 
+  len = sqrt(L[0]*L[0] + L[1]*L[1] + L[2]*L[2]) ;
+  if (len == 0) return 0 ;
+  L[0] /= len ;  L[1] /= len ;  L[2] /= len ;
+  double NdotL = N[0]*L[0] + N[1]*L[1] + N[2]*L[2] ;
+
+
+
+
+
+  double max_ambient_and_diffuse = AMBIENT + MAX_DIFFUSE ;
+     // this needs to occur BEFORE you possibly jump to LLL below
+
+
+
+
+  double intensity ;
+  if (NdotL*NdotE < 0) {
+    // eye and light are on opposite sides of polygon
+    intensity = AMBIENT ; 
+    goto LLL ;
+  } else if ((NdotL < 0) && (NdotE < 0)) {
+    // eye and light on same side but normal pointing "wrong" way
+    N[0] *= (-1.0) ;    N[1] *= (-1.0) ;    N[2] *= (-1.0) ; 
+    NdotE *= (-1.0) ;   // don't use NdotE below, probably should eliminate this
+  }
+
+
+  // ignore Blinn's variant
+  double R[3] ; // Reflection vector of incoming light
+  R[0] = 2*NdotL*N[0] - L[0] ;
+  R[1] = 2*NdotL*N[1] - L[1] ;
+  R[2] = 2*NdotL*N[2] - L[2] ;
+
+  double EdotR = E[0]*R[0] + E[1]*R[1] + E[2]*R[2] ;
+
+  double diffuse ;
+  if (NdotL <= 0.0) { diffuse = 0.0 ; }
+  else { diffuse = MAX_DIFFUSE*NdotL ; }
+
+  double specular ;
+  if (EdotR <= 0.0) { specular = 0.0 ; }
+  else { specular = (1.0 - max_ambient_and_diffuse)*pow(EdotR,SPECPOW) ;}
+
+  // printf("%lf %lf\n",diffuse,specular) ;
+  intensity = AMBIENT + diffuse + specular ;
+
+
+
+ LLL : ;
+
+  double f,g ;
+  if (intensity <= max_ambient_and_diffuse) {
+    f = intensity / max_ambient_and_diffuse ;
+    argb[0] = f * irgb[0] ;
+    argb[1] = f * irgb[1] ;
+    argb[2] = f * irgb[2] ;
+  } else {
+    f = (intensity - max_ambient_and_diffuse) / 
+                           (1.0 - max_ambient_and_diffuse) ;
+    g = 1.0 - f ;
+    argb[0] = g * irgb[0] + f ;
+    argb[1] = g * irgb[1] + f ;
+    argb[2] = g * irgb[2] + f ;
+  }
+
+  return 1 ;
+}
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+// to support graphing
+double win_width, win_height;
+double zbuff[MAX_WIN_WIDTH][MAX_WIN_HEIGHT];
+double screenTransform[4][4];
+int id; // screen output id
+
+int make_win_transform(double w, double h) {
+	
+	int Tn = 0;
+	int Ttypelist[100] ;
+	double Tvlist[100] ;
+	double Ti[4][4];
+
+	Ttypelist[Tn] = SX ; Tvlist[Tn] =  w / 2.0  ; Tn++ ;
+	Ttypelist[Tn] = SY ; Tvlist[Tn] =  h / 2.0 ; Tn++ ;
+	Ttypelist[Tn] = TX ; Tvlist[Tn] =  w / 2.0 ; Tn++ ;
+	Ttypelist[Tn] = TY ; Tvlist[Tn] =  h / 2.0 ; Tn++ ;
+	M3d_make_movement_sequence_matrix(screenTransform, Ti,   Tn,Ttypelist, Tvlist);
+
+	win_width = w;
+	win_height = h;
+}
+
+int make_z_buff() {
+	for(int i=0; i< win_width; ++i) {
+		for(int j=0; j<win_height; ++j) {
+			zbuff[i][j] = 9999999;
+		}
+	}
+}
+
+
+int plot(double P[3], double rgb[3], int id)
+{
+
+
+
+	
+
+
+	// scale to z=1 plane
+	P[0] /= P[2];
+	P[1] /= P[2];
+	double zbar = P[2];
+
+	// move to screen-space
+	M3d_mat_mult_pt(P, screenTransform, P);
+
+	int xbarbar = floor(P[0]);
+	int ybarbar = floor(P[1]);
+
+	// if it's closer to the camera than the previous point, go ahead and render it
+	// also make sure it's actually inside the window
+	if(zbar < zbuff[xbarbar][ybarbar] && 1) {
+	   //0 <= xbarbar && xbarbar < win_width &&
+	   //0 <= ybarbar && ybarbar < win_height) {
+	   	
+
+		int e = set_xwd_map_color(id, xbarbar, ybarbar,  rgb[0], rgb[1], rgb[2]);
+		zbuff[xbarbar][ybarbar] = zbar;
+	}
+
+	return 1;
+}
+
+
+
+int get_normal(double u, double v,
+	       double ustep, double vstep,
+	       int (*f)(double u, double v, double P[3]),
+	       double T[4][4],
+	       double N[3])
+{
+	double P[3], P2[3], P3[3];
+	if(!f(u, v, P)) { return 0; }
+	if(!f(u+ustep, v, P2)) { return 0; }
+	if(!f(u, v+vstep, P3)) { return 0; }
+
+	// get eye, P, and N
+	// transform points to camera-space
+	M3d_mat_mult_pt(P,  T, P);
+	M3d_mat_mult_pt(P2, T, P2);
+	M3d_mat_mult_pt(P3, T, P3);
+
+	// subtract P2-P, P3-P to get vectors, not points
+	M3d_vector_mult_const(P2, P2, -1);
+	M3d_vector_add(P2, P2, P);
+	M3d_vector_mult_const(P2, P2, -1);
+	
+	M3d_vector_mult_const(P3, P3, -1);
+	M3d_vector_add(P3, P3, P);
+	M3d_vector_mult_const(P3, P3, -1);
+
+
+	M3d_x_product(N, P2, P3);
+	return 1;
+
+}
+
+
+double calc_area_def(double u, double v,
+		   double ustep, double vstep,
+	  	   int (*f)(double u, double v, double P[3]),
+		   double T[4][4]
+	       )
+{
+	double N[3];
+	if(!get_normal(u,v,ustep,vstep,f,T,N)) { return 0; }
+	double mag1 = M3d_magnitude(N);
+	return mag1;
+
+}
+
+
+
+
+
+
+
+
+// not intended to be used outside of this file
+int make_graph_base(
+	       double ustart, double uend, double ustep,
+	       double vstart, double vend, double vstep,
+	       int (*f)(double u, double v, double P[3]),
+	       double T[4][4],
+	       int (*light_model_wrapper)(double u, double v,
+		       			 double eye[3], double P[3], double N[3],
+					 double rgb_out[3]),
+	       int id
+	       )
+{
+	double u, v;
+	double bigu, bigv;
+	double comp_area_def;
+
+
+	// keep trying random comparison points until one of them isn't 0.
+	srand(4242);
+	for(int i=0; i<100; ++i){
+		const double u_random = (uend-ustart) * (1.0*rand()/RAND_MAX) + ustart;
+		const double v_random = (vend-vstart) * (1.0*rand()/RAND_MAX) + vstart;
+		comp_area_def = calc_area_def(u_random, v_random, ustep,vstep, f, T);
+		if (comp_area_def == 0.0) {
+			break;
+		}
+	} 
+	if (comp_area_def == 0.0) {
+		comp_area_def = 1;
+	}
+
+	for(bigu = ustart; bigu < uend; bigu += ustep) {
+
+
+		for(bigv = vstart; bigv < vend; bigv += vstep) {
+
+			// refine to make sure the resolution is the same
+			double area_def = calc_area_def(bigu, bigv, ustep,vstep, f, T);
+			if(area_def * area_def == area_def) {
+				// nan, inf, or 0
+				continue;
+			} 
+			double ratio = area_def/comp_area_def;
+			//double ratio = 1;
+			if(ratio != 1) {
+				//printf("ratio: %i, area_def:%lf, comp_area_def:%lf\n", ratio, area_def, comp_area_def);
+			}
+			for(double u = bigu; u < bigu+ustep; u += ustep / ratio) {
+				for(double v = bigv; v < bigv+vstep; v += vstep / ratio) {
+
+	
+	
+					double P[3];
+					if(!f(u, v, P)) {
+						continue;
+					}
+					
+					// first, apply the given transformation to move into camera-space
+					M3d_mat_mult_pt(P, T, P);
+		
+					// don't bother rendering if point is behind camera
+					if(P[2] <= 0) { continue; } 		
+					// also, don't bother rendering if point is not in cone of vision
+					if(fabs(P[0]/P[2]) > 1) { continue; }
+					if(fabs(P[1]/P[2]) > 1) { continue; }
+		
+		
+		
+					// next, do the light model
+					double new_rgb[3];
+					double eye[3] = {0, 0, 0};
+					double N[3];
+		
+					if(!get_normal(u, v, ustep, vstep,
+						   f,
+						   T,
+						   N)) {
+						// error getting normal
+						continue;
+					}
+		
+					light_model_wrapper(u, v,   eye, P, N, new_rgb);
+					//Light_Model(inherent_rgb, eye, P, N, new_rgb);
+		
+		
+		
+					plot(P, new_rgb, id); 
+					//printf("u,v: (%lf, %lf)\n", u, v);
+	
+				}
+			}
+	
+		}
+
+	}
+
+}
+
+
+
+// using inherent rgbs per object:
+
+
+
+int make_graph_inherentrgb_1func(
+	       double ustart, double uend, double ustep,
+	       double vstart, double vend, double vstep,
+	       int (*f)(double u, double v, double P[3]),
+	       double T[4][4],
+	       double inherent_rgb[3],
+    	       int id)
+{
+	int rgb_light_model_wrapper(double u, double v,
+		       		   double eye[3], double P[3], double N[3],
+				   double new_rgb[3])
+	{
+		Light_Model(inherent_rgb, eye, P, N, new_rgb);
+	}
+	make_graph_base(
+			ustart,uend,ustep,
+			vstart,vend,vstep,
+			f,
+			T,
+			rgb_light_model_wrapper,
+			id);
+}
+
+
+int make_graph(
+	       double ustart, double uend, double ustep,
+	       double vstart, double vend, double vstep,
+	       double (*X)(double u, double v),
+	       double (*Y)(double u, double v),
+	       double (*Z)(double u, double v),
+	       double T[4][4],
+	       double inherent_rgb[3],
+	       int id)
+{
+
+
+	int f(double u, double v, double P[3]) {
+		P[0] = X(u, v);
+		P[1] = Y(u, v);
+		P[2] = Z(u, v);
+		return 1;
+	}
+
+	make_graph_inherentrgb_1func(
+			ustart,uend,ustep,
+			vstart,vend,vstep,
+			f,
+			T,
+			inherent_rgb,
+			id);
+}
+
+
+
+int make_graph_image_1func(
+	       double ustart, double uend, double ustep,
+	       double vstart, double vend, double vstep,
+	       int (*f)(double u, double v, double P[3]),
+	       double T[4][4],
+	       int image_ID, int image_width, int image_height,
+	       int id)
+{
+	int rgb_light_model_wrapper(double u, double v,
+		       		   double eye[3], double P[3], double N[3],
+				   double new_rgb[3])
+	{
+		double rgb[3];
+		int x = floor( image_width * (u-ustart) / (uend-ustart) );
+		int y = floor( image_height* (v-vstart) / (vend-vstart) );
+		int e = get_xwd_map_color(image_ID,  x,y,rgb);
+		if(e == -1) { return -1; }
+		Light_Model(rgb, eye, P, N, new_rgb);
+	}
+
+	make_graph_base(
+			ustart,uend,ustep,
+			vstart,vend,vstep,
+			f,
+			T,
+			rgb_light_model_wrapper,
+			id);
+}
+
+
+int make_graph_image(
+	       double ustart, double uend, double ustep,
+	       double vstart, double vend, double vstep,
+	       double (*X)(double u, double v),
+	       double (*Y)(double u, double v),
+	       double (*Z)(double u, double v),
+	       double T[4][4],
+	       int image_ID, int image_width, int image_height,
+	       int id)
+{
+
+
+	int f(double u, double v, double P[3]) {
+		P[0] = X(u, v);
+		P[1] = Y(u, v);
+		P[2] = Z(u, v);
+		return 1;
+	}
+
+	make_graph_image_1func(
+			ustart,uend,ustep,
+			vstart,vend,vstep,
+			f,
+			T,
+			image_ID, image_width, image_height,
+			id);
+}
+
+
